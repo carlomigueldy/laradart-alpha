9673c978a2aaa102f6c1d2f2dc15f997e908d7af
Carlo Miguel Dy <carlomigueldy@gmail.com>
2020-07-10 09:57:21 +0800

    feat(fallbackPage): added an unknown screen for when user encounters a screen that was not defined in the routes
    this screen will be thrown to the user

---
+++ lib/screens/unknown_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class UnknownScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child:
+            Text('Sorry but the screen you were looking for does not exist.'),
+      ),
+    );
+  }
+}
